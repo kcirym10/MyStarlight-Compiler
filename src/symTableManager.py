0612b8793dfcc8f0ca9689254f202b9befdffc2a
kcirym10 <kcirym10@me.com>
2022-04-20 22:34:16 -0500

    Added currentType tracking variable, setter function for currentType and changed getNewSymTable to set self as parent
    Minor changes to unit test which now make use of the new funcitonality

--- src/symTableManager.py
+++ src/symTableManager.py
@@ -9,12 +9,19 @@ from typing import List
 from symTable import symTable
 
 class symTableManager(List):
+    currentType = None
+
     def __init__(self):
         self.pushTable(symTable())
 
+    # Sets current type in case needed xD
+    def setCurrentType(self, type):
+        self.currentType = type
+
     # Returns a new table to be used either as a new scope or a new variables table
     def getNewSymTable(self):
-        return symTable()
+        newSymTable = symTable(self[-1])
+        return newSymTable
 
     # Pushes a new Symbol Table to the top of the stack
     # this could be a new scope (such as a function) or a vars table
@@ -31,9 +38,14 @@ if __name__ == "__main__":
     key = "Program"
     value1 = {'type': "Program Name", 'pRef': None, 'cRef': None}
     symMngr.insertRecord(key, value1)
-    newSymTable = symTable()
+    newSymTable = symMngr.getNewSymTable()
     value2 = {'type': "Program Name", 'pRef': symMngr[-1], 'cRef': newSymTable}
     symMngr.pushTable(newSymTable)
+    print(symMngr[-1].parentRef)
     symMngr.insertRecord(key, value2)
 
     print(symMngr)
+
+    # symMngr.insertRecord("key1", '1')
+    # symMngr.insertRecord("key2", '2')
+    # print(list(symMngr[-1].keys())[-1])
