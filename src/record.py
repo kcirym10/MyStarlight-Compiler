afdb2d9b726d4ebcb244200fdc38dcf22d23648e
Tanya González Elizondo <tanya_yaretzi@live.com.mx>
2022-04-20 19:34:59 -0500

    renamed the class as record, developed the setters for parameters and created a clear function for the current record

---
+++ src/record.py
@@ -0,0 +1,38 @@
+# Class that manages the dictionaries format
+
+class Record:
+
+    def __init__(self):
+        # has the current parameters of the record
+        self.currentRecord = {'type': None,
+                              'parentRef': None, 'childRef': None}
+
+    def setType(self, type):
+        self.currentRecord['type'] = str(type)
+
+    def setParentRef(self, parentRef):
+        self.currentRecord['parentRef'] = parentRef
+
+    def setChildRef(self, childRef):
+        self.currentRecord['childRef'] = childRef
+
+    def clearCurrentRecord(self):
+        self.currentRecord = {'type': None,
+                              'parentRef': None, 'childRef': None}
+
+    def returnRecord(self):
+        return self.currentRecord
+
+
+if __name__ == '__main__':
+    reg = Record()
+
+    reg.setType("int")
+    reg.setParentRef("none")
+    reg.setChildRef("vars")
+    x = reg.returnRecord()
+    print(x)
+
+    reg.clearCurrentRecord()
+    x = reg.returnRecord()
+    print(x)
