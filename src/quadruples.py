cfe44136046b76b91514dc07d955214892ccb8ea
kcirym10 <kcirym10@me.com>
2022-05-07 00:54:38 -0500

    Finished quadruple creation along with virtual memory structur and avail

--- src/quadruples.py
+++ src/quadruples.py
@@ -1,10 +1,13 @@
 from ast import operator
 from collections import deque
 
-from semanticCube import semanticCube
+from semanticCube import semantics
+from avail import Avail
 
 
 class Quadruples:
+    # Temporary Address Manager
+    avail = Avail()
 
     # Operators stack
     operatorStack = deque()
@@ -12,7 +15,7 @@ class Quadruples:
     operandStack = deque()
     # Types stack
     typeStack = deque()
-    cube = semanticCube()  # Should we create the object here (?)
+    #cube = semanticCube()  # Should we create the object here (?)
     pQuadruples = deque()
 
     def pushOperandType(self, operand, opType):
@@ -20,36 +23,41 @@ class Quadruples:
         self.typeStack.append(opType)
 
     def pushOperator(self, operator):
+        print(operator)
         self.operatorStack.append(operator)
 
     def createQuadruple(self, operator, left_operand, right_operand, temp):
-        quadruple = operator + left_operand + right_operand + temp
+        quadruple = (operator, left_operand, right_operand, temp)
         self.pQuadruples.append(quadruple)
+        print(f'{self.pQuadruples[-1][0]},\t{self.pQuadruples[-1][1]},\t{self.pQuadruples[-1][2]},\t{self.pQuadruples[-1][3]}')
 
     def createIfTopIs(self, operator):
         #If operator stack not empty
-        if self.pOperators:
+        if self.operatorStack:
             # Check top of the stack
-            oper = self.pOperators.pop()
-            self.pOperators.append(oper)
-            if(oper == operator or oper in operator):
-                oper = self.pOperators.pop()
-                '''right_operand = self.pOperands.pop()  # right operand
-                left_operand = self.pOperands.pop()  # left operand
-                right_type = self.pTypes.pop()
-                left_type = self.pTypes.pop()
+            oper = self.operatorStack.pop()
+            self.operatorStack.append(oper)
+            # print("In create")
+            if(oper in operator):
+                print(oper)
+                oper = self.operatorStack.pop()
+                # self.operatorStack.append(oper)
+                #print("Operator: ", oper)
+                right_operand = self.operandStack.pop()  # right operand
+                left_operand = self.operandStack.pop()  # left operand
+                right_type = self.typeStack.pop()
+                left_type = self.typeStack.pop()
 
+                # TODO: Implement Avail class. Must push new avail to operandStack and new type to typeStack
                 # Check the type of the temporal
-                tempType = self.cube.semantics(left_type, right_type, operator)
+                tempType = semantics(left_type, right_type, oper)
+                #print("TEMP TYPE: ",tempType)
                 if(tempType != None):
                     # Assign a memory space to temp
-                    temp = 5  # here goes avail(tempType)
-                    self.createQuadruple(oper, left_operand, right_operand, temp)
+                    temp = self.avail.next(tempType)
+                    self.createQuadruple(oper, str(left_operand), str(right_operand), str(temp))
                     # Add the temporal variable to the Operands stack
-                    self.pOperands.append(temp)
+                    self.operandStack.append(temp)
+                    self.typeStack.append(tempType)
                 else:
-                    print("Type Mismatch")'''
-            else:
-                self.pOperators.append(operator)
-        else:
-            self.pOperators.append(operator)
+                    print("Type Mismatch")
---
+++ src/virtualMemory.py
@@ -0,0 +1,26 @@
+memoryArchitecture = {
+    "GS" : { # Global Segment
+        "int" : 0,
+        "float" : 5000,
+        "char" : 10000,
+        "bool" : 12000
+    },
+    "LS" : { # Local Segment
+        "int" : 13000,
+        "float" : 18000,
+        "char" : 23000,
+        "bool" : 25000
+    },
+    "TS" : { # Temporary Segment
+        "int" : 27000,
+        "float" : 32000,
+        "char" : 37000,
+        "bool" : 39000
+    },
+    "CS" : { # Constant Segment
+        "int" : 41000,
+        "float" : 46000,
+        "char" : 51000,
+        "bool" : 53000
+    }
+}
