cc80320e6bb5016d2cdcfbf1dc8bc4d5ba1a9559
kcirym10 <kcirym10@me.com>
2022-05-07 02:26:14 -0500

    Added logic and reset functions based on deepcopying from original memory ranges

--- src/virtualMemory.py
+++ src/virtualMemory.py
@@ -1,3 +1,5 @@
+import copy
+
 memoryArchitecture = {
     "GS" : { # Global Segment
         "int" : 0,
@@ -24,3 +26,34 @@ memoryArchitecture = {
         "bool" : 53000
     }
 }
+
+class VirtualMemory:
+    memory = None
+    def __init__(self):
+        self.memory = copy.deepcopy(memoryArchitecture)
+
+    def nextGlobal(self, addressType):
+        address = self.memory["GS"][addressType]
+        self.memory["GS"][addressType] += 1
+        return address
+
+    def nextLocal(self, addressType):
+        address = self.memory["LS"][addressType]
+        self.memory["LS"][addressType] += 1
+        return address
+
+    def resetLocal(self):
+        self.memory["LS"] = copy.deepcopy(memoryArchitecture["LS"])
+
+    # def resetTemporary(self):
+    #     self.memory["TS"] = dict(memoryArchitecture["TS"])
+
+    # def nextAvail(self, addressType):
+    #     address = self.memory["TS"][addressType]
+    #     self.memory["TS"][addressType] += 1
+    #     return address
+
+    def nextConstant(self, addressType):
+        address = self.memory["CS"][addressType]
+        self.memory["CS"][addressType] += 1
+        return address
